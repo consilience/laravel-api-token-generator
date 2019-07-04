724d3874521840ed8c90d6546cfce2faa99d2ea9
Jason Judge <jason.judge@consil.co.uk>
2019-07-04 09:41:51 +0100

    Default token field name to api_token

--- config/apitokens.php
+++ config/apitokens.php
@@ -3,10 +3,12 @@
 return [
     // Changes behaviour of console command.
     // this tells the command which model to create the token against.
+    
     'model' => \App\User::class,
 
     // Field: Here's the field on the model in which you search with the `--value=` option.
     // Example: 'field' => 'email'
     // `php artisan apitoken:generate --value="example@app.com"
-    'field' => 'name'
+    
+    'field' => 'api_token'
 ];
